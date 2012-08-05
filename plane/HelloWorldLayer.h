00c031444f01f8b1a1319428489b2ab4980e1b65
wupeng <apple@ApplematoMacBook-Pro-2.local>
2012-08-05 20:34:25 +0800

    Initial Commit

---
+++ plane/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  plane
+//
+//  Created by wupeng on 12-8-5.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ plane/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  plane
+//
+//  Created by wupeng on 12-8-5.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
